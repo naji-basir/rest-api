daac46e14b17ef957418e3d4ad2bfad0f6044816
Basir Naji <basirnaji007@gmail.com>
2026-03-19 02:02:50 +0430

    routes created

---
+++ routes/routes.ts
@@ -0,0 +1,6 @@
+import { Express, Request, Response } from 'express';
+export const routes = (app: Express) => {
+  app.get('/healthcheck', (req: Request, res: Response) => {
+    res.sendStatus(200);
+  });
+};

cfffe243a9e3b2bd2ff5e0dcb0a3633d59827e47
Basir Naji <basirnaji007@gmail.com>
2026-03-19 02:17:21 +0430

    //

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,12 @@ const app = express();
 import config from 'config';
 import connect from './utils/connect';
 import logger from './utils/logger';
+import { routes } from '../routes/routes';
 
 const PORT = config.get<number>('port');
 app.listen(PORT, () => {
   logger.info(`App is running at http://localhost:${PORT}`);
 
   connect();
+  routes(app);
 });

7f22bb5e329fb8a0871257456b2a04018fb93bba
Basir Naji <basirnaji007@gmail.com>
2026-03-19 23:43:15 +0430

    moved

--- routes/routes.ts
+++
@@ -1,6 +0,0 @@
-import { Express, Request, Response } from 'express';
-export const routes = (app: Express) => {
-  app.get('/healthcheck', (req: Request, res: Response) => {
-    res.sendStatus(200);
-  });
-};
